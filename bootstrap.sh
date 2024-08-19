9d247deba5d751ee74559cc4bb0f8718fc80f8d0
Matt Wunschel <wunsmat@gmail.com>
2024-08-19 11:27:12 +0000

    Added auto login

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,5 +6,8 @@ pacman --noconfirm -S --needed - < /vagrant/pkglist.txt
 ln -fs /vagrant/.zshrc /home/vagrant/.zshrc
 ln -fs /vagrant/.zprofile /home/vagrant/.zprofile
 ln -fs -r /vagrant/.config /home/vagrant/.config
+cp -r /vagrant/getty@tty1.service.d /etc/systemd/system/
 
 chsh --shell /bin/zsh vagrant
+
+git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si
